3b52e3cfbf9b6609128984fcba5e03b2be4796de
Yaolin Ge <geyaolin@gmail.com>
2022-04-22 08:59:05 +0200

    update_new_plot

--- Simulation/Config/Config.py
+++ Simulation/Config/Config.py
@@ -5,7 +5,8 @@ Contact: yaolin.ge@ntnu.no
 Date: 2022-03-21
 """
 import os
-
+import numpy as np
+from usr_func import latlon2xy
 
 # == GP kernel
 THRESHOLD = 27
@@ -17,8 +18,6 @@ DISTANCE_SAME_LOCATION = 1
 
 # == Grid
 DISTANCE_NEIGHBOUR = 32
-LATITUDE_ORIGIN = 0
-LONGITUDE_ORIGIN = 0
 DEPTH_ORIGIN = 0
 DISTANCE_LATERAL = 32
 DISTANCE_VERTICAL = .5
@@ -32,9 +31,17 @@ FIGPATH = working_directory + "/MAFIA/fig/"
 FILEPATH = working_directory + "/MAFIA/"
 # ==
 
+# == Boundary box
+BOX = np.load(FILEPATH+"models/grid.npy")
+LAT_BOX = BOX[:, 2]
+LON_BOX = BOX[:, -1]
+LATITUDE_ORIGIN = LAT_BOX[0]
+LONGITUDE_ORIGIN = LON_BOX[0]
+xbox, ybox = latlon2xy(LAT_BOX, LON_BOX, LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
+ROTATED_ANGLE = np.math.atan2(xbox[1] - xbox[0], ybox[1] - ybox[0])
+# ==
+
 # == PLotting
 from matplotlib.cm import get_cmap
 CMAP = get_cmap("BrBG", 10)
 # ==
-
-
--- Simulation/PathPlanner/PathPlanner.py
+++ Simulation/PathPlanner/PathPlanner.py
@@ -74,6 +74,9 @@ class PathPlanner:
         self.knowledge.ind_previous_location_waypoint = self.knowledge.ind_current_location_waypoint
         self.knowledge.next_location = self.knowledge.current_location
 
+        # == set up rotational angle to make the plot easier
+        self.knowledge.rotated_angle = ROTATED_ANGLE
+
     def run(self):
         for i in range(self.num_steps):
             print("Step No. ", i)
@@ -87,18 +90,19 @@ class PathPlanner:
             print("ind_sample_grid: ", self.ind_sample_grid)
 
             self.knowledge.step_no = i
-            KnowledgePlot(knowledge=self.knowledge, vmin=0, vmax=32,
-                          filename=FILEPATH + "fig/myopic3d/P_{:03d}".format(i), html=False)
-
-            print("test of plotting")
-
             break
+            # KnowledgePlot(knowledge=self.knowledge, vmin=0, vmax=32,
+            #               filename=FILEPATH + "fig/myopic3d/P_{:03d}".format(i), html=False)
+
+            # print("test of plotting")
+
+
         # KnowledgePlot(knowledge=self.knowledge, vmin=VMIN, vmax=VMAX, filename=foldername + "Field_{:03d}".format(i), html=False)
 
 
 if __name__ == "__main__":
-    lat_start = 63.45121
-    lon_start = 10.40673
+    lat_start = 63.451197
+    lon_start = 10.411521
     depth_start = .5
     x, y = latlon2xy(lat_start, lon_start, LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
     z = depth_start
@@ -106,6 +110,10 @@ if __name__ == "__main__":
 
     p = PathPlanner(starting_location=starting_location)
     p.run()
+
+
+
+
 
 #%%
 
@@ -120,177 +128,187 @@ class KnowledgePlot:
         self.vmax = vmax
         self.filename = filename
         self.html = html
+
         # self.plot()
+        self.prepare_coordinates()
         self.simple_plot()
 
+    def prepare_coordinates(self):
+        self.ind_remove_top_layer = np.where(self.coordinates[:, 2]>0)[0]
+        xgrid = self.coordinates[self.ind_remove_top_layer, 0]
+        ygrid = self.coordinates[self.ind_remove_top_layer, 1]
+        xrotated = xgrid * np.cos(self.knowledge.rotated_angle) - ygrid * np.sin(self.knowledge.rotated_angle)
+        yrotated = xgrid * np.sin(self.knowledge.rotated_angle) + ygrid * np.cos(self.knowledge.rotated_angle)
+
+        self.xplot = yrotated
+        self.yplot = xrotated
+        self.zplot = self.coordinates[self.ind_remove_top_layer, 2]
+
+        pass
+
     def simple_plot(self):
-        x = self.coordinates[:, 0]
-        y = self.coordinates[:, 1]
-        z = self.coordinates[:, 2]
+
 
         fig = go.Figure(data=[go.Scatter3d(
-            x=x,
-            y=y,
-            z=-z,
+            x=self.xplot,
+            y=self.yplot,
+            z=-self.zplot,
             mode='markers',
             marker=dict(
                 size=12,
-                color=self.knowledge.mu_cond,  # set color to an array/list of desired values
+                color=self.knowledge.mu_cond[self.ind_remove_top_layer],  # set color to an array/list of desired values
                 colorscale='Viridis',  # choose a colorscale
                 opacity=0.8
             )
         )])
 
-        # tight layout
-        fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-        plotly.offline.plot(fig, filename=FIGPATH+"test_plot.html", auto_open=True)
+        camera = dict(
+            up=dict(x=0, y=0, z=1),
+            center=dict(x=0, y=0, z=0),
+            eye=dict(x=2.25, y=2.25, z=2.25)
+        )
+        fig.update_layout(
+            title={
+                'text': "Simulation",
+                'y': 0.9,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+            scene=dict(
+                zaxis=dict(nticks=4, range=[-10, 0], ),
+                xaxis_tickfont=dict(size=14, family="Times New Roman"),
+                yaxis_tickfont=dict(size=14, family="Times New Roman"),
+                zaxis_tickfont=dict(size=14, family="Times New Roman"),
+                xaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                yaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
+                zaxis_title=dict(text="Z", font=dict(size=18, family="Times New Roman")),
+            ),
+            scene_aspectmode='manual',
+            scene_aspectratio=dict(x=1, y=1, z=1),
+            scene_camera=camera,
+        )
+
+        plotly.offline.plot(fig, filename=self.filename + ".html", auto_open=False)
 
 
     def plot(self):
-        x = self.coordinates[:, 0]
-        y = self.coordinates[:, 1]
-        z = self.coordinates[:, 2]
+        x = self.coordinates[self.ind_remove_top_layer, 1]
+        y = self.coordinates[self.ind_remove_top_layer, 0]
+        z = self.coordinates[self.ind_remove_top_layer, 2]
         z_layer = np.unique(z)
         number_of_plots = len(z_layer)
 
         # print(lat.shape)
-        points_mean, values_mean = interpolate_3d(y, x, z, self.knowledge.mu_cond)
+        # points_mean, values_mean = interpolate_3d(y, x, z, self.knowledge.mu_cond[self.ind_remove_top_layer])
         # points_std, values_std = interpolate_3d(y, x, z, np.sqrt(self.knowledge.Sigma_cond_diag))
-        points_ep, values_ep = interpolate_3d(y, x, z, self.knowledge.excursion_prob)
+        # points_ep, values_ep = interpolate_3d(y, x, z, self.knowledge.excursion_prob[self.ind_remove_top_layer])
+
+        points_mean = self.coordinates
+        values_mean = self.knowledge.mu_cond[self.ind_remove_top_layer]
+
+        points_ep = self.coordinates
+        values_ep = self.knowledge.excursion_prob[self.ind_remove_top_layer]
 
         trajectory = []
         for i in range(len(self.knowledge.trajectory)):
-            trajectory.append([self.knowledge.trajectory[i].x,
-                               self.knowledge.trajectory[i].y,
+            trajectory.append([self.knowledge.trajectory[i].y,
+                               self.knowledge.trajectory[i].x,
                                self.knowledge.trajectory[i].z])
 
         fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'scene'}, {'type': 'scene'}]],
                             subplot_titles=("Updated field", "Updated excursion probability",))
         # fig = make_subplots(rows = 1, cols = 3, specs = [[{'type': 'scene'}, {'type': 'scene'}, {'type': 'scene'}]],
         #                     subplot_titles=("Conditional Mean", "Std", "EP"))
-        fig.add_trace(go.Volume(
-            x=points_mean[:, 0],
-            y=points_mean[:, 1],
-            z=-points_mean[:, 2],
-            value=values_mean.flatten(),
-            isomin=self.vmin,
-            isomax=self.vmax,
-            opacity=.1,
-            surface_count=30,
-            colorscale="BrBG",
-            # coloraxis="coloraxis1",
-            colorbar=dict(x=0.5, y=0.5, len=.5),
-            reversescale=True,
-            caps=dict(x_show=False, y_show=False, z_show=False),
+        # fig.add_trace(go.Volume(
+        fig.add_trace(go.Scatter3d(
+            x=x,
+            y=y,
+            z=-z,
+            # x=points_mean[:, 1],
+            # y=points_mean[:, 0],
+            # z=-points_mean[:, 2],
+            mode='markers',
+            marker=dict(
+                size=12,
+                color=self.knowledge.mu_cond[self.ind_remove_top_layer],  # set color to an array/list of desired values
+                colorscale='BrBG',  # choose a colorscale
+                opacity=0.8
+            ),
         ),
             row=1, col=1
         )
-        # print(values_std)
-        # if len(values_std):
-        #     fig.add_trace(go.Volume(
-        #         x=points_std[:, 0],
-        #         y=points_std[:, 1],
-        #         z=-points_std[:, 2],
-        #         value=values_std.flatten(),
-        #         isomin=0,
-        #         isomax=1,
-        #         opacity=.1,
-        #         surface_count=30,
-        #         colorscale = "rdbu",
-        #         colorbar=dict(x=0.65, y=0.5, len=.5),
-        #         reversescale=True,
-        #         caps=dict(x_show=False, y_show=False, z_show=False),
-        #     ),
-        #         row=1, col=2
-        #     )
 
-        fig.add_trace(go.Volume(
-            x=points_ep[:, 0],
-            y=points_ep[:, 1],
-            z=-points_ep[:, 2],
-            value=values_ep.flatten(),
-            isomin=0,
-            isomax=1,
-            opacity=.1,
-            surface_count=30,
-            colorscale="gnbu",
-            colorbar=dict(x=1, y=0.5, len=.5),
-            reversescale=True,
-            caps=dict(x_show=False, y_show=False, z_show=False),
+        fig.add_trace(go.Scatter3d(
+            x=x,
+            y=y,
+            z=z,
+            # x=points_ep[:, 1],
+            # y=points_ep[:, 0],
+            # z=-points_ep[:, 2],
+            mode='markers',
+            marker=dict(
+                size=12,
+                color=self.knowledge.excursion_prob[self.ind_remove_top_layer],  # set color to an array/list of desired values
+                # colorscale='gnbu',  # choose a colorscale
+                opacity=0.8
+            ),
         ),
             row=1, col=2,
-            # row = 1, col = 3,
         )
 
-        if len(self.knowledge.ind_neighbour_filtered_waypoint):
-            fig.add_trace(go.Scatter3d(
-                x=self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 0],
-                y=self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 1],
-                z=-self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 2],
-                mode='markers',
-                marker=dict(
-                    size=15,
-                    color="white",
-                    showscale=False,
-                ),
-                showlegend=False,
-            ),
-                row='all', col='all'
-            )
-
-        # if len(self.knowledge.ind_cand_filtered):
+        # if len(self.knowledge.ind_neighbour_filtered_waypoint):
         #     fig.add_trace(go.Scatter3d(
-        #         x=self.knowledge.coordinates[self.knowledge.ind_cand_filtered, 1],
-        #         y=self.knowledge.coordinates[self.knowledge.ind_cand_filtered, 0],
-        #         z=-self.knowledge.coordinates[self.knowledge.ind_cand_filtered, 2],
+        #         x=self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 1],
+        #         y=self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 0],
+        #         z=-self.knowledge.coordinates_waypoint[self.knowledge.ind_neighbour_filtered_waypoint, 2],
         #         mode='markers',
         #         marker=dict(
-        #             size=10,
-        #             color="blue",
+        #             size=15,
+        #             color="white",
         #             showscale=False,
         #         ),
-        #         showlegend=False, # remove all unnecessary trace names
+        #         showlegend=False,
         #     ),
         #         row='all', col='all'
         #     )
 
-        if trajectory:
-            trajectory = np.array(trajectory)
-            fig.add_trace(go.Scatter3d(
-                # print(trajectory),
-                x=trajectory[:, 0],
-                y=trajectory[:, 1],
-                z=-trajectory[:, 2],
-                mode='markers+lines',
-                marker=dict(
-                    size=5,
-                    color="black",
-                    showscale=False,
-                ),
-                line=dict(
-                    color="yellow",
-                    width=3,
-                    showscale=False,
-                ),
-                showlegend=False,
-            ),
-                row='all', col='all'
-            )
-
-        fig.add_trace(go.Scatter3d(
-            x=[self.knowledge.current_location.x],
-            y=[self.knowledge.current_location.y],
-            z=[-self.knowledge.current_location.z],
-            mode='markers',
-            marker=dict(
-                size=20,
-                color="red",
-                showscale=False,
-            ),
-            showlegend=False,  # remove all unnecessary trace names
-        ),
-            row='all', col='all'
-        )
+        # if trajectory:
+        #     trajectory = np.array(trajectory)
+        #     fig.add_trace(go.Scatter3d(
+        #         # print(trajectory),
+        #         x=trajectory[:, 0],
+        #         y=trajectory[:, 1],
+        #         z=-trajectory[:, 2],
+        #         mode='markers+lines',
+        #         marker=dict(
+        #             size=5,
+        #             color="black",
+        #             showscale=False,
+        #         ),
+        #         line=dict(
+        #             color="yellow",
+        #             width=3,
+        #             showscale=False,
+        #         ),
+        #         showlegend=False,
+        #     ),
+        #         row='all', col='all'
+        #     )
+        #
+        # fig.add_trace(go.Scatter3d(
+        #     x=[self.knowledge.current_location.y],
+        #     y=[self.knowledge.current_location.x],
+        #     z=[-self.knowledge.current_location.z],
+        #     mode='markers',
+        #     marker=dict(
+        #         size=20,
+        #         color="red",
+        #         showscale=False,
+        #     ),
+        #     showlegend=False,  # remove all unnecessary trace names
+        # ),
+        #     row='all', col='all'
+        # )
 
         camera = dict(
             up=dict(x=0, y=0, z=1),
@@ -306,34 +324,34 @@ class KnowledgePlot:
                 'xanchor': 'center',
                 'yanchor': 'top'},
             scene=dict(
-                zaxis=dict(nticks=4, range=[-3, 0], ),
+                zaxis=dict(nticks=4, range=[-10, 0], ),
                 xaxis_tickfont=dict(size=14, family="Times New Roman"),
                 yaxis_tickfont=dict(size=14, family="Times New Roman"),
                 zaxis_tickfont=dict(size=14, family="Times New Roman"),
-                xaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
-                yaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                xaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                yaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
                 zaxis_title=dict(text="Z", font=dict(size=18, family="Times New Roman")),
             ),
             scene_aspectmode='manual',
             scene_aspectratio=dict(x=1, y=1, z=.5),
             scene2=dict(
-                zaxis=dict(nticks=4, range=[-3, 0], ),
+                zaxis=dict(nticks=4, range=[-10, 0], ),
                 xaxis_tickfont=dict(size=14, family="Times New Roman"),
                 yaxis_tickfont=dict(size=14, family="Times New Roman"),
                 zaxis_tickfont=dict(size=14, family="Times New Roman"),
-                xaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
-                yaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                xaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                yaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
                 zaxis_title=dict(text="Z", font=dict(size=18, family="Times New Roman")),
             ),
             scene2_aspectmode='manual',
             scene2_aspectratio=dict(x=1, y=1, z=.5),
             scene3=dict(
-                zaxis=dict(nticks=4, range=[-3, 0], ),
+                zaxis=dict(nticks=4, range=[-10, 0], ),
                 xaxis_tickfont=dict(size=14, family="Times New Roman"),
                 yaxis_tickfont=dict(size=14, family="Times New Roman"),
                 zaxis_tickfont=dict(size=14, family="Times New Roman"),
-                xaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
-                yaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                xaxis_title=dict(text="Y", font=dict(size=18, family="Times New Roman")),
+                yaxis_title=dict(text="X", font=dict(size=18, family="Times New Roman")),
                 zaxis_title=dict(text="Z", font=dict(size=18, family="Times New Roman")),
             ),
             scene3_aspectmode='manual',
@@ -390,4 +408,46 @@ kp = KnowledgePlot(knowledge=self.knowledge, vmin=0, vmax=32,
 
 
 
+#%%
+grid = np.load(FILEPATH+"models/grid.npy")
+lats = np.load(FILEPATH+"models/lats.npy")
+lons = np.load(FILEPATH+"models/lons.npy")
+depth = np.load(FILEPATH+"models/depth.npy")
+
+lat_box = grid[:, 2]
+lon_box = grid[:, 3]
+
+lat_origin = lat_box[0]
+lon_origin = lon_box[0]
+
+x, y = latlon2xy(lat_box, lon_box, lat_origin, lon_origin)
+angle = np.math.atan2(x[1] - x[0], y[1] - y[0])
+import math
+
+plt.plot(y, x)
+plt.plot(y[0], x[0], 'o')
+plt.plot(y[1], x[1], 'x')
+plt.show()
+# angle = np.math.atan2(lat_box[1] - lat_box[0], lon_box[1] - lon_box[0])
+angle = np.math.atan2(x[1] - x[0], y[1] - y[0])
+print("Angle is: ", math.degrees(angle))
+
+plt.figure(figsize=(10, 10))
+# plt.plot(lons, lats, 'k.')
+# plt.show()
+
+
+xgrid, ygrid = latlon2xy(lats, lons, lat_origin, lon_origin)
+plt.plot(ygrid, xgrid, 'g.')
+plt.show()
+
+#%%
+# angle = math.radians(30)
+xp = xgrid * np.cos(angle) - ygrid * np.sin(angle)
+yp = xgrid * np.sin(angle) + ygrid * np.cos(angle)
+
+latsp, lonsp = xy2latlon(xp, yp, lat_origin, lon_origin)
+
+plt.plot(lonsp, latsp, 'r.')
+plt.show()
 
