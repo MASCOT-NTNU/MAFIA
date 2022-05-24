68bd73f1af3891ee4e38fd00e3ff27ccdc4ff4b8
Yaolin Ge <geyaolin@gmail.com>
2022-05-24 15:59:32 +0200

    update_eda

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,75 +1,162 @@
-import numpy as np
-import pandas as pd
+"""
+This script does simple EDA analysis
+Author: Yaolin Ge
+Contact: yaolin.ge@ntnu.no
+Date: 2022-05-13
+"""
+import matplotlib.pyplot as plt
 
-from DataHandler.SINMOD import SINMOD
 from usr_func import *
-FILEPATH = "/Users/yaolin/HomeOffice/MAFIA/EDA/"
-FIGPATH = FILEPATH + "fig/"
-# FILEPATH = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/"
-# FIGPATH = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Projects/MAFIA/EDA/fig/"
-# data_auv = pd.read_csv(FILEPATH + "AUVData.csv")
-# lat_auv = data_auv['lat'].to_numpy()
-# lon_auv = data_auv['lon'].to_numpy()
-# depth_auv = data_auv['depth'].to_numpy()
-# coordinates_auv = np.vstack((lat_auv, lon_auv, depth_auv)).T
-# salinity_auv = data_auv['salinity'].to_numpy()
-# df = pd.DataFrame(np.vstack((lat_auv, lon_auv, depth_auv, salinity_auv)).T, columns=['lat', 'lon', 'depth', 'salinity'])
-# df.to_csv(FILEPATH + "auv.csv", index=False)
+from MAFIA.EDA.Config.Config import *
+from DataHandler.SINMOD import SINMOD
+from skgstat import Variogram
 
-# sinmod = SINMOD()
-# filenames_fullpath = ["/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/SINMOD_DATA/samples_2021.05.27.nc"]
-# sinmod.load_sinmod_data(raw_data=True, filenames=filenames_fullpath)
-# sinmod.get_data_at_coordinates(coordinates_auv, filename="/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Projects/MAFIA/EDA/sinmod.csv")
 
-data_auv = pd.read_csv(FILEPATH + "auv.csv").to_numpy()
-data_sinmod = pd.read_csv(FILEPATH + "sinmod.csv").to_numpy()
+DATAPATH = "/Users/yaolin/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/20220511/MAFIA/"
+SINMODPATH = "/Users/yaolin/HomeOffice/MAFIA/Experiments/20220511/"
+FIGPATH = "/Users/yaolin/HomeOffice/MAFIA/fig/Experiments/20220511/"
+# AUV_TIMESTEP = 170
+# == Set up
+LAT_START = 63.456232
+LON_START = 10.435198
+
+
+class EDA:
+
+    def __init__(self):
+        self.load_auv_data()
+        # self.load_grfar_model()
+        # self.load_rrtstar()
+        # self.load_cost_valley()
+
+    def load_auv_data(self):
+        self.data_auv = pd.read_csv(DATAPATH + "data_sync.csv").to_numpy()
+        self.timestamp_auv = self.data_auv[:, 0]
+        self.lat_auv = self.data_auv[:, 1]
+        self.lon_auv = self.data_auv[:, 2]
+        self.depth_auv = self.data_auv[:, 3]
+        self.salinity_auv = self.data_auv[:, 4]
+        self.temperature_auv = self.data_auv[:, 5]
+        print("AUV data is loaded successfully!")
+
+    def load_sinmod_data(self, data_exists=True):
+        if not data_exists:
+            self.sinmod = SINMOD()
+            self.sinmod.load_sinmod_data(raw_data=True)
+            coordinates_auv = np.vstack((self.lat_auv, self.lon_auv, self.depth_auv)).T
+            self.sinmod.get_data_at_coordinates(coordinates_auv)
+        else:
+            self.data_sinmod = pd.read_csv(SINMODPATH+"data_sinmod.csv")
+            print("SINMOD data is loaded successfully!")
+            print(self.data_sinmod.head())
+            self.data_sinmod = self.data_sinmod.to_numpy()
+        pass
+
+    def plot_scatter_data(self):
+        fig = go.Figure(data=go.Scatter3d(
+            x=self.lon_auv,
+            y=self.lat_auv,
+            z=-self.depth_auv,
+            mode='markers',
+            marker=dict(color=self.data_auv[:, 3], size=10)
+        ))
+        plotly.offline.plot(fig, filename=FILEPATH+"fig/EDA/samples.html", auto_open=True)
+        pass
+
+    def plot_2d(self):
+        plt.scatter(self.lon_auv, self.lat_auv, c=self.salinity_auv, cmap=get_cmap("BrBG", 10), vmin=22, vmax=self.CV.threshold)
+        plt.colorbar()
+        plt.show()
+
+
+if __name__ == "__main__":
+    e = EDA()
+    e.load_sinmod_data(data_exists=False)
+    # e.plot_prior()
+    # e.plot_sinmod_on_grf_grid()
+    # e.plot_sinmod_on_grf_grid()
+    # e.plot_2d()
+    # e.get_residual_with_sinmod()
+    # e.plot_scatter_data()
+    # e.plot_recap_mission()
+    # e.plot_variogram()
+
+ #%%
+# plt.scatter(e.grf_grid[:, 1], e.grf_grid[:, 0], c=e.grfar_model.mu_prior, cmap=get_cmap("BrBG", 10), vmin=10, vmax=27)
+# plt.colorbar()
+# plt.plot(LON_START, LAT_START, 'rs')
+# plt.show()
+
+
+
 
 
 #%%
 
-from pathlib import Path
-fig = go.Figure(data=[go.Scatter3d(
-    x=data_auv[:, 1],
-    y=data_auv[:, 0],
-    z=-data_auv[:, 2],
-    mode='markers',
-    marker=dict(
-        size=8,
-        color=data_auv[:, 3],                # set color to an array/list of desired values
-        colorscale='BrBG',   # choose a colorscale
-        opacity=0.8
-    )
-)])
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import plotly
+import netCDF4
+from datetime import datetime
+from matplotlib.cm import get_cmap
+import re
 
+LATITUDE_ORIGIN = 63.4269097
+LONGITUDE_ORIGIN = 10.3969375
+CIRCUMFERENCE = 40075000 # [m], circumference
+circumference = CIRCUMFERENCE
+def latlon2xy(lat, lon, lat_origin, lon_origin):
+    x = np.deg2rad((lat - lat_origin)) / 2 / np.pi * CIRCUMFERENCE
+    y = np.deg2rad((lon - lon_origin)) / 2 / np.pi * CIRCUMFERENCE * np.cos(np.deg2rad(lat))
+    return x, y
 
-# tight layout
-fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-figname = FIGPATH+"data_auv.html"
-plotly.offline.plot(fig, filename=figname, auto_open=False)
-os.system('open -a \"Google Chrome\" '+figname)
+file = "/Users/yaolin/Library/CloudStorage/OneDrive-NTNU/MASCOT_PhD/Data/Nidelva/SINMOD_DATA/samples_2022.05.11.nc"
+FIGPATH = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220511/fig/"
+# file = "/Users/yaolin/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/SINMOD_DATA/samples_2022.05.10.nc"
+# figpath = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220509/fig/"
 
+sinmod = netCDF4.Dataset(file)
+ind_before = re.search("samples_", file)
+ind_after = re.search(".nc", file)
+date_string = file[ind_before.end():ind_after.start()]
+ref_timestamp = datetime.strptime(date_string, "%Y.%m.%d").timestamp()
+timestamp = np.array(sinmod["time"]) * 24 * 3600 + ref_timestamp #change ref timestamp
+lat_sinmod = np.array(sinmod['gridLats'])
+lon_sinmod = np.array(sinmod['gridLons'])
+depth_sinmod = np.array(sinmod['zc'])
+salinity_sinmod = np.array(sinmod['salinity'])
+
+# for i in range(salinity_sinmod.shape[0]):
+#     print(i)
+#     plt.figure(figsize=(12, 10))
+#     plt.scatter(lon_sinmod, lat_sinmod, c=salinity_sinmod[i, 0, :, :], cmap=get_cmap("BrBG", 8), vmin=10, vmax=self.CV.threshold)
+#     plt.xlabel("Lon [deg]")
+#     plt.ylabel("Lat [deg]")
+#     plt.title("SINMOD Surface Salinity Estimation on " + datetime.fromtimestamp(timestamp[i]).strftime("%H:%M, %Y-%m-%d"))
+#     plt.colorbar()
+#     plt.savefig(figpath+"sinmod/P_{:03d}.jpg".format(i))
+#     plt.close("all")
 
 #%%
-fig = go.Figure(data=[go.Scatter3d(
-    x=data_sinmod[:, 1],
-    y=data_sinmod[:, 0],
-    z=-data_sinmod[:, 2],
-    mode='markers',
-    marker=dict(
-        size=8,
-        color=data_sinmod[:, 3],                # set color to an array/list of desired values
-        colorscale='BrBG',   # choose a colorscale
-        opacity=0.8
-    )
-)])
+lat_grid, lon_grid = xy2latlon(e.grf_grid[:, 0], e.grf_grid[:, 1], LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
+sal_mean = np.mean(salinity_sinmod[:, 0, :, :], axis=0)
+# plt.figure(figsize=(12, 10))
+# plt.scatter(lon_sinmod, lat_sinmod, c=sal_mean, cmap=get_cmap("BrBG", 8), vmin=10, vmax=30)
+# plt.xlabel("Lon [deg]")
+# plt.ylabel("Lat [deg]")
+# plt.show()
 
-# tight layout
-fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-figname = FIGPATH+"data_sinmod.html"
-plotly.offline.plot(fig, filename=figname, auto_open=False)
-os.system('open -a \"Google Chrome\" '+figname)
+plt.scatter(lon_grid, lat_grid, c=e.grfar_model.mu_sinmod, cmap=get_cmap("BrBG", 8), vmin=10, vmax=30)
+# plt.plot(lon_grid, lat_grid, 'y.', alpha=.3)
+plt.colorbar()
+plt.title("SINMOD")
+# plt.savefig(figpath+"sinmod_grid.jpg")
+plt.show()
 
-#%%
+
+
 
 
 
